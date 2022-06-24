859372cad8d89c3f766ef34b5d03d62a02419590
T-Betrand <tebobertrand@gmail.com>
2022-06-24 16:32:48 +0100

    test

--- vue.config.js
+++ vue.config.js
@@ -3,14 +3,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  devServer: {
-    proxy: {
-      '^/api': {
-        target: '',
-        changeOrigin: true,
-        logLevel: 'debug',
-        pathRewrite: {'^/api': '/'},
-      }
-    }
-  }
+  // devServer: {
+  //   proxy: {
+  //     '^/api': {
+  //       target: '',
+  //       changeOrigin: true,
+  //       logLevel: 'debug',
+  //       pathRewrite: {'^/api': '/'},
+  //     }
+  //   }
+  // }
 })
